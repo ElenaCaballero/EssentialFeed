8107fb41f78bee6c6dd17b60e00be6a12874cee7
Elena Caballero <elena.caballero96@gmail.com>
2024-02-19 10:08:48 +0100

    Creating RemoteFeedLoader and HTTPClient protocol

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -5,4 +5,29 @@
 //  Created by Elena Gabriela Caballero Murillo on 19.02.24.
 //
 
-import Foundation
+import XCTest
+
+class RemoteFeedLoader {
+    let client: HTTPClient
+    let url: URL
+    
+    init(client: HTTPClient, url: URL) {
+        self.client = client
+        self.url = url
+    }
+    
+    func load() {
+        client.get(from: url)
+    }
+}
+
+protocol HTTPClient {
+    func get(from url: URL)
+}
+
+class RemoteFeedLoaderTests: XCTestCase {
+    
+    func test_init() {
+        
+    }
+}
