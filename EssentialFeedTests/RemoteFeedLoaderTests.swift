a7f1c82bfbac2bf4cb1aac1546f0a0914fac1e7b
Elena Caballero <elena.caballero96@gmail.com>
2024-02-19 10:12:25 +0100

    Creating helpers

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -11,7 +11,7 @@ class RemoteFeedLoader {
     let client: HTTPClient
     let url: URL
     
-    init(client: HTTPClient, url: URL) {
+    init(url: URL, client: HTTPClient) {
         self.client = client
         self.url = url
     }
@@ -29,5 +29,20 @@ class RemoteFeedLoaderTests: XCTestCase {
     
     func test_init() {
         
+    }
+    
+    // MARK: - Helpers
+    private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
+        let client = HTTPClientSpy()
+        let sut = RemoteFeedLoader(url: url, client: client)
+        return (sut, client)
+    }
+    
+    private class HTTPClientSpy: HTTPClient {
+        var requestedURL: URL?
+        
+        func get(from url: URL) {
+            requestedURL = url
+        }
     }
 }
