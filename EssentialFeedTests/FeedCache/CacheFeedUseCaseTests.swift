dedfcb073af96b459c8d562828812966b962b22f
Elena Caballero <elena.caballero96@gmail.com>
2024-04-26 14:53:42 +0200

    Test does not delete cache upon creation

---
+++ EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
@@ -0,0 +1,29 @@
+//
+//  CacheFeedUseCaseTests.swift
+//  EssentialFeedTests
+//
+//  Created by Elena Gabriela Caballero Murillo on 26.04.24.
+//
+
+import XCTest
+
+class LocalFeedLoader {
+    init(store: FeedStore) {
+        
+    }
+}
+
+class FeedStore {
+    var deleteCachedFeedCallCount = 0
+}
+
+class CacheFeedUseCaseTests: XCTestCase {
+    
+    func test_init_doesNotDeleteCacheUponCreation() {
+        let store = FeedStore()
+        _ = LocalFeedLoader(store: store)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
+    }
+    
+}
