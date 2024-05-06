de3c2f36ca714fba9bbdcf02b8781dad28941dce
Elena Caballero <elena.caballero96@gmail.com>
2024-05-06 11:55:17 +0200

    Load command requests cache retrieval

--- EssentialFeedTests/FeedCache/Helpers/FeedStoreSpy.swift
+++ EssentialFeedTests/FeedCache/Helpers/FeedStoreSpy.swift
@@ -12,12 +12,15 @@ class FeedStoreSpy: FeedStore {
     enum ReceivedMessage: Equatable {
         case deleteCachedFeed
         case insert([LocalFeedImage], Date)
+        case retrieve
     }
     
     private(set) var receivedMessages = [ReceivedMessage]()
     
     private var deletionCompletions = [DeletionCompletion]()
     private var insertionCompletions = [InsertionCompletion]()
+    
+    // MARK: - Delete cached feed
     
     func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         deletionCompletions.append(completion)
@@ -32,6 +35,8 @@ class FeedStoreSpy: FeedStore {
         deletionCompletions[index](nil)
     }
     
+    // MARK: - Insert feed
+    
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
         insertionCompletions.append(completion)
         receivedMessages.append(.insert(feed, timestamp))
@@ -43,5 +48,11 @@ class FeedStoreSpy: FeedStore {
     
     func completeInsertionSuccessfully(at index: Int = 0) {
         insertionCompletions[index](nil)
+    }
+    
+    // MARK: - Retrieve feed
+    
+    func retrieve() {
+        receivedMessages.append(.retrieve)
     }
 }
